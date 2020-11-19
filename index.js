8672692bfd8d5549d646aa1f2ff33b17420bd1fd
Alex <alexhenderson@gmail.com>
2020-11-18 20:57:46 -0500

    Nav bar

---
+++ index.js
@@ -0,0 +1,41 @@
+const fetchPage = (file) => {
+	fetch(file)
+  		.then(function(response) {
+    		return response.text();
+  		})
+  		.then(function(body) {
+    		document.querySelector('#content').innerHTML = body;
+  		});
+}
+
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li')
+
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active');
+		burger.classList.toggle('openBurger');
+
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = "";
+			}
+			else {
+				link.style.animation = `navLinksFade 0.5s ease forwards ${(index / 5) + 0.3}s`;
+			}
+		})
+	});	
+
+	navLinks.forEach((link, index) => {
+		link.addEventListener('click', () => {
+			var file = link.textContent.replaceAll(" ", "") + ".html";
+			fetchPage(file);
+		})
+	})
+}
+
+(function () {
+    navSlide();
+    fetchPage("home.html");
+})();
