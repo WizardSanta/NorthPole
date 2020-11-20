85ecea9c048b77e6f1b83d8840285054d1cf56de
Alex <alexhenderson@gmail.com>
2020-11-20 12:29:21 -0500

    add content

---
+++ music.js
@@ -0,0 +1,14 @@
+// var getSoundCloudLinks = () => {
+// 	SC.initialize({
+// 	  client_id: 'YOUR_CLIENT_ID'
+// 	});
+
+// 	var track_url = 'https://soundcloud.com/wizardsanta/embark-beach';
+// 	SC.oEmbed(track_url, { auto_play: false }).then(function(oEmbed) {
+// 	  console.log('oEmbed response: ', oEmbed);
+// 	});
+// }
+
+// (function () {
+//     getSoundCloudLinks();
+// })();
